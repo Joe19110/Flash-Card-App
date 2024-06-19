e291e55cacc323780de1d64d2a7b2f9803ed13a7
Joe19110 <137983830+Joe19110@users.noreply.github.com>
2024-06-20 00:08:50 +0700

    Delete src directory

--- src/DefaultScrollPanel.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-import java.awt.*;
-
-public class DefaultScrollPanel extends JScrollPane {
-    public DefaultScrollPanel(Component view) {
-        super(view);
-        setBackground(DefaultFrame.systemColor);
-    }
-}
